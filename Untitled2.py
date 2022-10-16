e481a18ae59ef38eeb10cf07f318efef54a85337
Abhi2876 <115937884+Abhi2876@users.noreply.github.com>
2022-10-16 21:04:59 +0530

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[5]:
+
+
+pd.read_excel(C:\Users\abhis\OneDrive\Desktop\Book2.xlsx)
+
+
+# In[6]:
+
+
+pd
+
+
+# In[7]:
+
+
+pd.read_csv('https://raw.githubusercontent.com/shivam229/dsrscientist-DSData/master/Concrete_Data.csv')
+
+
+# In[ ]:
+
+
+
+
